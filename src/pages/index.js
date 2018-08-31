c30385c48290cfe0dfa80de03c04a99da0cf0465
Murrium123 <murriumlovesyou@hotmail.ca>
2018-08-30 21:37:40 -0300

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi everyone!</h1>
+    <p>I am Murrium and I am a beginnner coder who is going to compete in San Francisco with you all. Woo hoo!</p>
+    <p>Check out this awesome picture of me on page two</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
